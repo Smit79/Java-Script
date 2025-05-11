dc153a40c5ff61532fbfdc0c1e2f745db037957f
Smit79 <smitvansjaliya79@gmail.com>
2025-05-11 17:14:00 +0000

    variable study

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
-console.log("Hello word")
+const accountId = 112435
+let accountEmail = "Jayverma234@gmail.com"
+var accountPassword = "47895"
+accountCity = "Mumbai"
+
+// accountId = 50
+accountEmail = "Jayverma746@gmail.com"
+accountPassword = "95135"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity]);
