82e53973e012aef2a70d2a81272e4f62c4172960
Smit79 <smitvansjaliya79@gmail.com>
2025-05-12 17:43:34 +0000

    Stack and Heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,24 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//  -------------- Memory Reference ( Stack or Heap ) ------------------------ //
+let animal = "Dog"
+
+let anotheranimal = animal;
+anotheranimal = "Cat";
+
+console.log(animal);
+console.log(anotheranimal);
+
+let userone = {
+    userId : "XYZ",
+    password : 1672
+}
+
+let usertwo = userone
+
+usertwo.password = 9546;
+
+console.log(userone.password);
+console.log(usertwo.password);
