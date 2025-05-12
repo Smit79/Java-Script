20a720c586a00a3feab10ee270bf691a99a72adc
Smit79 <smitvansjaliya79@gmail.com>
2025-05-12 14:30:13 +0000

    comparisions

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -12,3 +12,34 @@ console.log(valueInNumber);
 // true => 1; and false => 0
 // 1 => true , 0 => false
 // "" => false , "somthing " => true
+
+// ******************************************  Operation *************************************** //
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// let str1 = "I am ";
+// let str2 = "King ";
+// console.log(str1 + str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"")
+
+let num = 100;
+num++;
+console.log(num);
+++num;
+console.log(num);
